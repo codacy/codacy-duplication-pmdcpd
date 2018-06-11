4e833fa1b72956c6f16ee36ddc47ddc0bc4a6ac6
Pedro Amaral <pamaral@codacy.com>
2018-06-11 14:37:19 +0100

    added tests and test resources

---
+++ src/test/resources/codacy/duplication/pmd/java/KeyboardReader.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+import foo.bar.baz
+import foo1.bar.baz
+import foo2.bar.baz
+import foo3.bar.baz
+import foo4.bar.baz
+
+public class KeyboardReader
+{
+
+    public static void main (String[] args) throws java.io.IOException
+    {
+
+        String s1;
+        String s2;
+
+        double num1, num2, product;
+
+        // set up the buffered reader to read from the keyboard
+        BufferedReader br = new BufferedReader (new InputStreamReader (
+                System.in));
+
+        System.out.println ("Enter a line of input");
+
+        s1 = br.readLine();
+
+        System.out.println ("The line has " + s1.length() + " characters");
+
+        System.out.println ();
+        System.out.println ("Breaking the line into tokens we get:");
+
+        int numTokens = 0;
+        StringTokenizer st = new StringTokenizer (s1);
+
+        while (st.hasMoreTokens())
+        {
+            s2 = st.nextToken();
+            numTokens++;
+            System.out.println ("    Token " + numTokens + " is: " + s2);
+        }
+    }
+}
---
+++ src/test/scala/com/codacy/duplication/pmd/CpdSpec.scala
@@ -0,0 +1,169 @@
+package com.codacy.duplication.pmd
+
+import codacy.docker.api.duplication.{DuplicationClone, DuplicationCloneFile}
+import codacy.docker.api.{DuplicationConfiguration, Source}
+import com.codacy.api.dtos.{Language, Languages}
+import org.specs2.matcher.MatchResult
+import org.specs2.mutable.Specification
+
+import _root_.scala.collection.GenTraversableOnce
+import _root_.scala.util.{Success, Try}
+
+class CpdSpec extends Specification {
+
+  val targetDir = "src/test/resources"
+
+  "Cpd" should {
+    "get duplication" in {
+      " without defining a language (directory has scala files)" in noLanguageTest()
+      " have 1 clone in 2 Scala files" in scalaTest()
+      " have 2 clones in 2 Java files" in javaTest()
+      " have 2 clones in 2 Python files" in pythonTest()
+      " have clones in 2 Javascript files" in javascriptTestGeneric()
+      " not have clones in comments" in javascriptTestComments()
+      " have 2 clones in 2 CSharp files" in cSharpTest()
+    }
+  }
+
+  def executeDuplication(dir: String,
+                         language: Option[Language],
+                         options: Map[DuplicationConfiguration.Key,
+                                      DuplicationConfiguration.Value] =
+                           Map.empty): Try[List[DuplicationClone]] = {
+    Cpd(path = Source.Directory(dir), language = language, options)
+  }
+
+  def javascriptTestGeneric(): MatchResult[Try[List[DuplicationClone]]] = {
+    val codePath = s"$targetDir/codacy/duplication/pmd/javascript/misc"
+    val clonesTry: Try[List[DuplicationClone]] =
+      executeDuplication(codePath, Some(Languages.Javascript))
+
+    clonesTry must beSuccessfulTry
+
+    clonesTry must beLike {
+      case Success(clones) => {
+        clones should haveLength(1)
+
+        val clone = clones.head
+        testClone(clone)(codePath, 194, 428, 2)
+      }
+    }
+  }
+
+  def javascriptTestComments(): MatchResult[Try[List[DuplicationClone]]] = {
+    val commentsPath = s"$targetDir/codacy/duplication/pmd/javascript/comments"
+    val clonesTry: Try[List[DuplicationClone]] =
+      executeDuplication(commentsPath, Some(Languages.Javascript))
+
+    clonesTry must beSuccessfulTry
+
+    clonesTry must beLike {
+      case Success(clones) =>
+        clones must haveLength(1)
+
+        val clone = clones.head
+        testClone(clone)(commentsPath, 18, 98, 2)
+    }
+  }
+
+  def noLanguageTest(): MatchResult[Try[List[DuplicationClone]]] = {
+    val codePath = s"$targetDir/codacy/duplication/pmd/scala"
+    val clonesTry =
+      executeDuplication(codePath, None, Map.empty)
+
+    clonesTry must beSuccessfulTry
+
+    clonesTry must beLike {
+      case Success(clones) =>
+        clones must haveLength(1)
+
+        val clone = clones.head
+        testClone(clone)(codePath, 21, 134, 2)
+    }
+  }
+
+  def scalaTest(): MatchResult[Try[List[DuplicationClone]]] = {
+    val codePath = s"$targetDir/codacy/duplication/pmd/scala"
+    val clonesTry =
+      executeDuplication(codePath, Some(Languages.Scala), Map.empty)
+
+    clonesTry must beSuccessfulTry
+
+    clonesTry must beLike {
+      case Success(clones) =>
+        clones must haveLength(1)
+
+        val clone = clones.head
+        testClone(clone)(codePath, 21, 134, 2)
+    }
+  }
+
+  def javaTest(): MatchResult[Try[List[DuplicationClone]]] = {
+    val codePath = s"$targetDir/codacy/duplication/pmd/java"
+    val clonesTry =
+      executeDuplication(codePath, Some(Languages.Java), Map.empty)
+
+    clonesTry should beSuccessfulTry
+
+    clonesTry must beLike {
+      case Success(clones) =>
+        clones must haveLength(1)
+
+        val clone = clones.head
+        testClone(clone)(codePath, 28, 115, 2)
+
+    }
+  }
+
+  def pythonTest(): MatchResult[Try[List[DuplicationClone]]] = {
+
+    val codePath = s"$targetDir/codacy/duplication/pmd/python"
+    val clonesTry =
+      executeDuplication(codePath, Some(Languages.Python))
+
+    clonesTry should beSuccessfulTry
+
+    clonesTry must beLike {
+      case Success(clones) =>
+        clones must haveLength(2)
+        testClone(clones.head)(codePath, 31, 159, 2)
+        testClone(clones.drop(1).head)(codePath, 33, 69, 2)
+    }
+  }
+
+  private def testClone(clone: DuplicationClone)(
+      dir: String,
+      nrLines: Int,
+      nrTokens: Int,
+      filesNr: Int): MatchResult[GenTraversableOnce[DuplicationCloneFile]] = {
+
+    clone.nrLines must beEqualTo(nrLines)
+    clone.nrTokens must beEqualTo(nrTokens)
+    clone.files must haveLength(filesNr)
+    forall(clone.files) { file =>
+      (file.filePath must not startWith "/") and
+        (file.filePath must not contain dir)
+    }
+  }
+
+  def cSharpTest(): MatchResult[Try[List[DuplicationClone]]] = {
+
+    val codePath = s"$targetDir/codacy/duplication/pmd/csharp"
+    val clonesTry =
+      executeDuplication(codePath, Some(Languages.CSharp))
+
+    clonesTry should beSuccessfulTry
+
+    clonesTry must beLike {
+      case Success(clones) =>
+        clones must haveLength(2)
+
+        val clone = clones.head
+        testClone(clone)(codePath, 24, 66, 2)
+
+        val secondClone = clones(1)
+        testClone(secondClone)(codePath, 9, 53, 2)
+    }
+  }
+
+}
