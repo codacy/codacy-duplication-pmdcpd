28dce54c69ceb982cfbaaa8ebac4f9cc97368dfb
José Miguel  Melo <josemiguelmelo94@gmail.com>
2020-01-28 14:03:06 +0000

    test: Add Cpp, Go and Java duplication tests

---
+++ src/main/resources/docs/duplication-tests/java-no-results/src/KeyboardReader.java
@@ -0,0 +1,9 @@
+package com.codacy.duplication.pmd.java;
+
+public class KeyboardReader
+{
+    public static void main (String[] args) throws java.io.IOException
+    {
+        System.out.println ("The line has " + s1.length() + " characters");
+    }
+}
