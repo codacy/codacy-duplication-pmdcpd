7c04a651a56303ac8e1e95ff6520d6491bbabdf5
david.marinho@codacy.com <david.marinho@codacy.com>
2025-03-12 17:51:27 +0000

    add new languages

--- src/main/scala/com/codacy/duplication/pmd/Cpd.scala
+++ src/main/scala/com/codacy/duplication/pmd/Cpd.scala
@@ -2,14 +2,26 @@ package com.codacy.duplication.pmd
 
 import _root_.java.io.{ByteArrayOutputStream, PrintStream}
 import _root_.java.nio.charset.StandardCharsets
-import _root_.java.nio.file.{Path, Paths}
+import _root_.java.nio.file.{Files, Path, Paths}
+import java.util.stream.Collectors
 
 import better.files._
 import com.codacy.docker.api.duplication._
 import com.codacy.plugins.api.duplication.{DuplicationClone, DuplicationCloneFile, DuplicationTool}
 import com.codacy.plugins.api.languages.{Language, Languages}
 import com.codacy.plugins.api.{Options, Source}
-import net.sourceforge.pmd.cpd.{Language => CPDLanguage, _}
+import net.sourceforge.pmd.cpd._
+import net.sourceforge.pmd.lang.{Language => CPDLanguage}
+import net.sourceforge.pmd.lang.cpp._
+import net.sourceforge.pmd.lang.cs._
+import net.sourceforge.pmd.lang.ecmascript._
+import net.sourceforge.pmd.lang.go._
+import net.sourceforge.pmd.lang.java._
+import net.sourceforge.pmd.lang.plsql._
+import net.sourceforge.pmd.lang.python._
+import net.sourceforge.pmd.lang.ruby._
+import net.sourceforge.pmd.lang.swift._
+import net.sourceforge.pmd.lang.scala._
 
 import _root_.scala.collection.JavaConverters._
 import _root_.scala.util.{Failure, Success, Try}
@@ -23,17 +35,21 @@ object Cpd extends DuplicationTool {
 
   private val allLanguages: List[Language] =
     List[Language](
+      Languages.Apex,
       Languages.CSharp,
       Languages.C,
       Languages.CPP,
       Languages.Javascript,
+      Languages.JSP,
       Languages.Go,
-      Languages.Java,
-      Languages.PLSQL,
+      Languages.Swift,
       Languages.Python,
-      Languages.Ruby,
       Languages.Scala,
-      Languages.Swift)
+      Languages.XML,
+      Languages.PLSQL,
+      Languages.TSQL,
+      Languages.Velocity,
+      Languages.VisualForce)
 
   override def apply(path: Source.Directory,
                      language: Option[Language],
@@ -47,13 +63,15 @@ object Cpd extends DuplicationTool {
 
     val cpdResultsTry = resolveLanguages(language).map { languages =>
       languages.flatMap { language =>
-        val files = File(directoryPath)
-          .walk()
-          .filter(
-            file =>
-              file.isRegularFile &&
-                language.extensions.contains(file.extension.getOrElse(file.name)) &&
-                file.size < MaxFileSize)
+        val files: Seq[Path] = Files
+          .walk(directoryPath)
+          .filter { path =>
+            Files.isRegularFile(path) && // Check if it's a regular file
+            language.extensions.exists(ext => path.getFileName.toString.endsWith(ext)) && // Check extension
+            Files.size(path) < MaxFileSize // Check file size
+          }
+          .collect(Collectors.toList())
+          .asScala
           .toSeq
 
         val configuration = resolveConfiguration(language, options)
@@ -78,11 +96,13 @@ object Cpd extends DuplicationTool {
 
   private def runWithConfiguration(config: CPDConfiguration,
                                    directory: Path,
-                                   files: Seq[File]): List[DuplicationClone] = {
-    val cpd = new CPD(config)
-    cpd.add(files.map(_.toJava).asJava)
-    cpd.go()
-    cpd.getMatches.asScala.map(duplicationClone(_, directory)).toList
+                                   files: Seq[Path]): List[DuplicationClone] = {
+    val cpd = CpdAnalysis.create(config)
+    files.foreach(file => cpd.files().addFile(file))
+    cpd.performAnalysis();
+    //falta getMatches
+    //val matches = cpd.getMatches().asScala.map(duplicationClone(_, directory)).toList
+    //return matches
   }
 
   private def resolveLanguages(language: Option[Language]): Try[List[Language]] = {
@@ -100,23 +120,16 @@ object Cpd extends DuplicationTool {
 
   private def resolveConfiguration(language: Language, options: Map[Options.Key, Options.Value]): CPDConfiguration = {
     language match {
-      case Languages.CSharp            => cpdConfiguration(new CsLanguage, 50, options)
-      case Languages.C | Languages.CPP => cpdConfiguration(new CPPLanguage(), 50, options)
-      case Languages.Javascript        => cpdConfiguration(new EcmascriptLanguage, 40, options)
-      case Languages.Go                => cpdConfiguration(new GoLanguage, 40, options)
-      case Languages.Java              => cpdConfiguration(new JavaLanguage, 100, options)
-      case Languages.PLSQL             => cpdConfiguration(new PLSQLLanguage, 100, options)
-      case Languages.Python            => cpdConfiguration(new PythonLanguage, 50, options)
-      case Languages.Ruby              => cpdConfiguration(new RubyLanguage, 50, options)
-      case Languages.Swift             => cpdConfiguration(new SwiftLanguage, 50, options)
-      case Languages.Scala =>
-        val cpdScala = new ScalaLanguage
-        val scalaLanguage = new AbstractLanguage(
-          cpdScala.getName,
-          cpdScala.getTerseName,
-          com.codacy.duplication.pmd.ScalaTokenizer,
-          cpdScala.getExtensions.asScala.toSeq: _*) {}
-        cpdConfiguration(scalaLanguage, 50, options)
+      case Languages.CSharp            => cpdConfiguration(new CsLanguageModule, 50, options)
+      case Languages.C | Languages.CPP => cpdConfiguration(new CppLanguageModule, 50, options)
+      case Languages.Javascript        => cpdConfiguration(new EcmascriptLanguageModule, 40, options)
+      case Languages.Go                => cpdConfiguration(new GoLanguageModule, 40, options)
+      case Languages.Java              => cpdConfiguration(new JavaLanguageModule, 100, options)
+      case Languages.PLSQL             => cpdConfiguration(new PLSQLLanguageModule, 100, options)
+      case Languages.Python            => cpdConfiguration(new PythonLanguageModule, 50, options)
+      case Languages.Ruby              => cpdConfiguration(new RubyLanguageModule, 50, options)
+      case Languages.Swift             => cpdConfiguration(new SwiftLanguageModule, 50, options)
+      case Languages.Scala             => cpdConfiguration(new ScalaLanguageModule, 50, options)
       case other =>
         throw new Exception(s"$other Language not supported")
     }
@@ -131,19 +144,15 @@ object Cpd extends DuplicationTool {
     val ignoreAnnotations = options.getValue[Boolean]("ignoreAnnotations").orElse(Some(true))
     val ignoreUsings = options.getValue[Boolean]("ignoreUsings").orElse(Some(true))
     val ignoreIdentifiers = options.getValue[Boolean]("ignoreIdentifiers").orElse(Some(false))
-    val skipLexicalErrors = options.getValue[Boolean]("skipLexicalErrors").orElse(Some(true))
     // This is mandatory in PMDCPD
     val minimumTileSize = options.getValue[Int]("minTokenMatch").getOrElse(defaultMinToken)
 
     val cfg = new CPDConfiguration()
-    cfg.setLanguage(cpdLanguage)
     ignoreAnnotations.foreach(cfg.setIgnoreAnnotations)
-    skipLexicalErrors.foreach(cfg.setSkipLexicalErrors)
     ignoreIdentifiers.foreach(cfg.setIgnoreIdentifiers)
     ignoreLiterals.foreach(cfg.setIgnoreLiterals)
     ignoreUsings.foreach(cfg.setIgnoreUsings)
     cfg.setMinimumTileSize(minimumTileSize)
-    CPDConfiguration.setSystemProperties(cfg)
     cfg
   }
 
